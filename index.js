a8f1ae0ef2933ca5b1fa61ede4aa34356207f3b1
Pavel Serikov <pavelsr@cpan.org>
2019-12-27 20:34:42 +0300

    Some fixes

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 var csv_filename = 'stocks_rates.csv';
 
+function get_tradingview_link(ticker, price_str) {
+	var exchange = 'MOEX';
+	if ( price_str.match(/\$/) ) {
+		console.log( price_str );
+		exchange = 'NASDAQ'
+	}
+	return 'https://ru.tradingview.com/symbols/'+exchange+'-'+ticker;
+	// https://ru.tradingview.com/symbols/NASDAQ-YNDX/
+	// https://ru.tradingview.com/symbols/MOEX-YNDX/
+}
+
 function createTable(results, callback) {
 	var table = $('table#jquery-table');
     table.empty();
 	
 	var thead_tr = $('<tr>');
 	$.each(results.meta.fields, function(x, col_h) {
+		if ( col_h == "URL" ) {
+			return true;
+		}
 	    $('<td>').text(col_h).appendTo(thead_tr);
 	});
+	
 	table.append( $('<thead>').append(thead_tr) );	
     
 	var tbody = $('<tbody>');
     $.each(results.data, function(x, row) {
         var tr = $('<tr>');
-        $.each(row, function(y, col) {
-            tr.append( $('<td>').text(col) );
+        $.each(row, function(col_name, col_value) {
+			if ( col_name == "Компания" ) {
+				tr.append( $('<td>').append('<a target="_blank" href="'+row["URL"]+ '">'+col_value+'</a>') );
+			}
+			else if ( col_name == "URL" ) {
+				return true;
+			}
+			else {
+            	tr.append( $('<td>').text(col_value) );
+			}
         });
+		tr.append( $('<td>').append('<a target="_blank" href="'+ get_tradingview_link(row["Тикер"],row["Цена текущая"]) + '">ссылка</a>') );
         tbody.append(tr);
     });
 
@@ -44,11 +68,17 @@ function TableSort() {
 	console.log("tablesorter finished");
 }
 
-Papa.parse(csv_filename, {
-	download: true,
-	header: true,
-	complete: function(results) {
-		console.log("Parsing complete:", results.data);
-		createTable(results, TableSort);
-	}
+$.get( csv_filename, function( data, textStatus, request ) {
+	$("span#file_date").text( request.getResponseHeader("Last-Modified") );
+	Papa.parse(data, {
+		header: true,
+		skipEmptyLines: true,
+		complete: function(results) {
+			results.meta.fields.push('Теханализ');
+			console.log("Parsing complete:", results.data);
+			createTable(results, TableSort);
+		}
+	});
 });
+
+
